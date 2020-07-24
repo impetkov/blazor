7063f8203cdb971eff9c1d37b3cffb0c42174e76
Ivan Petkov <ipetkov@appliedsystems.com>
2020-07-24 17:48:18 +0100

    First commit - created a simple blazor app

---
+++ TodoItem.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorApp
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set;  }
+
+        public TodoItem(string title)
+        {
+            Title = title;
+            IsDone = false;
+        }
+    }
+}
---
+++ TodoItems.cs
@@ -0,0 +1,27 @@
+﻿namespace BlazorApp
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class TodoItems : IEnumerable<TodoItem>
+    {
+        private readonly List<TodoItem> inner;
+
+        public TodoItems()
+        {
+            inner = new List<TodoItem>();
+        }
+
+        public void Add(string title)
+        {
+            var toAdd = new TodoItem(title);
+            inner.Add(toAdd);
+        }
+
+        public IEnumerator<TodoItem> GetEnumerator() =>
+            inner.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() =>
+            GetEnumerator();
+    }
+}
